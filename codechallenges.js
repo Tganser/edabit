d65a4aa082a6100d519e0c99800f63935d9af0f4
Tessa Ganser <t.m.ganser@gmail.com>
2017-05-23 08:43:50 -0500

    first commit

---
+++ codechallenges.js
@@ -0,0 +1,17 @@
+function isEvenOrOdd(num) {
+  if (num%2  === 0){
+    return "even";
+  }
+  else {
+    return "odd";
+  }
+}
+
+
+function makeTitle(str){
+  var words = str.split(" ");
+  for (i=0; i>str.length; i++){
+    i[0].capitalize();
+  }
+  return words;
+}
